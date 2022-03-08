278d94f7071d69e457824ee006068d02abcbc7f5
Chelsea Huang <cx3huang@uwaterloo.ca>
2022-03-07 22:48:37 -0500

    Q3 done

--- Q4/dynamics.m
+++ Q4/dynamics.m
@@ -12,6 +12,16 @@ soleus_activation = 0.05;
 tibialis_activation = 0.4;
 ankle_inertia = 90;
 
+soleus_moment_arm = 0.05;
+tibialis_moment_arm = 0.03;
+
 %WRITE CODE HERE TO IMPLEMENT THE MODEL
+
+tau_s = fm_s*force_length_tendon(x(3))*soleus_moment_arm;
+tau_ta = fm_ta*force_length_tendon(x(4))*tibialis_moment_arm;
+
+
+x1_dot = x(2);
+x2_dot = ()
 
 end
